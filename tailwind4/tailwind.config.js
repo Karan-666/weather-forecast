99c16c231a823e9ba519ff0ba33fef8e66303a29
Karan-666 <karankumar00619@gmail.com>
2025-09-10 14:14:37 +0530

    first commit with tailwind 4

---
+++ tailwind4/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: [
+    "./*.html",    // html in root folder
+    "../*.html"    // html in parent folder 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
